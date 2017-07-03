95683feed401ea831f94f08b8a0c6ed83dd79c54
Michael Noel <miphnoel@gmail.com>
2017-07-03 15:40:42 -0400

    Create production build settings

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,15 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
-
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 
 import RootReducer from '../reducers/root_reducer';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { createLogger } = require('redux-logger');
+  middlewares.push(createLogger());
+}
 
 const configureStore = (preLoadedState = {}) => (
   createStore(
     RootReducer,
     preLoadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 );
 
