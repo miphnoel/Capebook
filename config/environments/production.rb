53d2b9272484cca09f701630ffbd75ee916dc7ce
Michael Noel <miphnoel@gmail.com>
2017-07-19 01:00:47 -0400

    Update cloudfront subdomain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+  'Cache-Control' => 'public, max-age=31536000'
+}
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -28,7 +31,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'dssra5iv431o5.cloudfront.net'
+  config.action_controller.asset_host = 'd2fm9vftq6x0kn.cloudfront.net'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
