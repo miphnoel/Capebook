1d89c2607a260d55a01f316febafb230e70d0fcd
Michael Noel <miphnoel@gmail.com>
2017-07-26 17:38:46 -0400

    Remove unnecessary method import

--- frontend/capebook.jsx
+++ frontend/capebook.jsx
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { updateProfile } from './actions/user_actions';
-
 document.addEventListener("DOMContentLoaded", () => {
   let store;
   if (window.currentUser) {
@@ -15,8 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
